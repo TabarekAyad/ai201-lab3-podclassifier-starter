04c398de6fd440325ffadd5a2a27f8fe8105e2e1
TabarekAyad <tabarek.ayad@gmail.com>
2026-06-27 21:15:30 -0400

    commented line of old llm model

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 # --- LLM ---
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# LLM_MODEL = "llama-3.3-70b-versatile" hit rate limit
 LLM_MODEL = os.getenv("LLM_MODEL", "llama-3.1-8b-instant")
 LLM_MAX_TOKENS = int(os.getenv("LLM_MAX_TOKENS", "80"))
 
